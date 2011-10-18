e2e1ec17dd5ee225fb39b4d23fea397092badffa
Jacek Bzdak <jbzdak@gmail.com>
2011-10-18 13:32:58 +0200

    Bugfixes

--- sql-builder-api/src/main/java/cx/ath/jbzdak/sqlbuilder/ParametrizedValue.java
+++ sql-builder-api/src/main/java/cx/ath/jbzdak/sqlbuilder/ParametrizedValue.java
@@ -11,19 +11,31 @@ public class ParametrizedValue<T> {
 
    final String parameterType;
 
+   final boolean required;
+
    public ParametrizedValue(String parameterType) {
       this.parameterType = parameterType;
+      this.required = true;
    }
 
-   Boolean valueSet = Boolean.TRUE; //
+   public ParametrizedValue(String parameterType, boolean required) {
+      this.parameterType = parameterType;
+      this.required = required;
+   }
+
+   Boolean valueSet = Boolean.FALSE; //
 
    Parameter<? extends T> parameterValue;
 
    T value;
 
+
    public T getValue(ExpressionContext context){
       if(valueSet){
          return value;
+      }
+      if(parameterValue == null){
+         return null;
       }
       return context.resolveParameter(parameterValue);
    }
@@ -31,30 +43,43 @@ public class ParametrizedValue<T> {
 
 
    public void setValue(ExpressionContext context, T value){
-      valueSet = true;
-      this.value = value;
+      if(value!=null){
+         valueSet = true;
+         this.value = value;
+      }
+
    }
 
    public void setParameter(ExpressionContext context, Parameter<T> parameter){
       valueSet = false;
       parameterValue = parameter;
-      context.addParameter(parameter);
+      context.addParameter(parameter, false);
    }
 
    public void setFromString(ExpressionContext context, String string){
       if(context.isParameter(string)){
-         setParameter(context, context.getDialect().createParameter(string, parameterType));
+         setParameter(context, createParameter(context, string));
          return;
       }
       setValue(context, convert(context, string));
 
    }
 
+   private Parameter createParameter(ExpressionContext context, String string) {
+      Parameter parameter = context.getDialect().createParameter(string, parameterType);
+      parameter.setRequired(required);
+      return parameter;
+   }
+
    public T convert(ExpressionContext context, String value){
       if(value==null){
          return null;
       }
-      Parameter<T> parameter = context.getDialect().createParameter(value, parameterType);
+      Parameter<T> parameter = createParameter(context, value);
       return parameter.fromString(value);
    }
+
+
 }
+
+
